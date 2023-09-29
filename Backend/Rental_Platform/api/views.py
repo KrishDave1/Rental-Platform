b952b042a0d570e131e3696f1c1998d5f428c720
valmikGit <quantumcomputingiiitb@gmail.com>
2023-09-29 19:17:47 +0530

    Added api, base to the folder

---
+++ Backend/Rental_Platform/api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def get_Data(request):
+    person = {"name" : "Valmik", "age" : 19}
+    return Response(person)
