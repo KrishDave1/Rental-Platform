fc92ee0c40e471ea7e05ea944995fcc8202eccb6
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-09-30 02:16:37 +0530

    Added Context API and tested it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,19 @@ import { Link } from "react-router-dom";
 import { IoLocationOutline } from "react-icons/io5";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsPersonFill } from "react-icons/bs";
+import { useGlobalContext } from "../context";
+
 export const Navbar = () => {
+  const { dummy } = useGlobalContext();
+  const hello = () => {
+    console.log(dummy);
+  };
   return (
     <nav>
       <ul>
         <Link to="/">
           <li>Home</li>
+          <button onClick={hello}>Click me</button>
         </Link>
         <Link to="" />
         <li>
