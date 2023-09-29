3b53f7b56fc2eeadb53d3e755dfda81166a9069f
valmikGit <quantumcomputingiiitb@gmail.com>
2023-09-29 19:39:03 +0530

    Created base app and rest api in progress

--- Backend/Rental_Platform/api/views.py
+++ Backend/Rental_Platform/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_Data(request):
-    person = {"name" : "Valmik", "age" : 19}
+    person = {"name": "Valmik", "age": 19}
     return Response(person)
