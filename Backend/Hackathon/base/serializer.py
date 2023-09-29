d13a93a5b0da799b9ab7ff2318348f47f71f5586
valmikGit <quantumcomputingiiitb@gmail.com>
2023-09-29 19:11:59 +0530

    I removed everything

--- Backend/Hackathon/base/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Room
-
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ['name', 'description', 'updated', 'created']
