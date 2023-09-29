9ba7953262a319c9e3b830dfc278d55ef5cb3864
valmikGit <quantumcomputingiiitb@gmail.com>
2023-09-30 00:54:12 +0530

    Added filters.

--- Backend/Rental_Platform/base/models.py
+++ Backend/Rental_Platform/base/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# class City(models.Model):
+#     city_Name = 
+
+class Listing(models.Model):
+    type_Of_Item = models.CharField(max_length=100)
+    price = models.IntegerField()
+    duration_Of_Rent = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.type_Of_Item
+
 class User(models.Model):
     name = models.CharField(max_length=200)
     city_Rent = models.CharField(max_length=50)
@@ -8,9 +20,3 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return self.name
-
-class Listing(models.Model):
-    type_Of_Item = models.CharField(max_length=100)
-    price = models.IntegerField()
-    duration_Of_Rent = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
