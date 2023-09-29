3170006baebeceec330007d17a78fda926c5628c
valmikGit <quantumcomputingiiitb@gmail.com>
2023-09-29 20:44:25 +0530

    REST API in progress

--- Backend/Rental_Platform/api/urls.py
+++ Backend/Rental_Platform/api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('', views.get_Data),
-    path('add/', views.add_Item)
+    path('', views.item_List)
 ]
--- Backend/Rental_Platform/api/views.py
+++ Backend/Rental_Platform/api/views.py
@@ -3,15 +3,14 @@ from rest_framework.decorators import api_view
 from base.models import Item
 from .serializer import Item_Serializer
 
-@api_view(["GET"])
-def get_Data(request):
-    items = Item.objects.all()
-    serializer = Item_Serializer(items, many = True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def add_Item(request):
-    serializer = Item_Serializer(data=request.data)
-    if(serializer.is_valid()):
-        serializer.save()
-    return Response(serializer.data)
+@api_view(['GET', 'POST'])
+def item_List(request):
+    if(request.method == 'GET'):
+        items = Item.objects.all()
+        serializer = Item_Serializer(items, many = True)
+        return Response(serializer.data)
+    if(request.method == 'POST'):
+        serializer = Item_Serializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+        return Response(serializer.data)
