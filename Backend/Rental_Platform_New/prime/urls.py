f194a3d632d37a13bd478574380026a34edb5227
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-01 18:10:06 +0530

    Made a basic rest api.

--- Backend/Rental_Platform_New/prime/urls.py
+++ Backend/Rental_Platform_New/prime/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from prime import views
 
 urlpatterns = [
-    
+    path('', view=views.user_List)
 ]
--- Backend/Rental_Platform_New/prime/views.py
+++ Backend/Rental_Platform_New/prime/views.py
@@ -12,6 +12,7 @@ from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
 
 User = get_user_model()
 
@@ -55,3 +56,17 @@ def sign_Up_User(request):
         else:
             messages.error(request=request, message="Your password and confirm password do not match.")
     return render(request=request, template_name="sign_Up.html")
+
+@api_view(['GET', 'POST'])
+def user_List(request):
+    if(request.method == "GET"):
+        items = CustomUser.objects.all()
+        serializer = CustomUser_Serializer(items, many=True)
+        return Response(serializer.data)
+    if(request.method == "POST"):
+        serializer = CustomUser_Serializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
